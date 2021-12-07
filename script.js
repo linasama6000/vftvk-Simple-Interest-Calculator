d98276a329ab4b299441d3b4b6bd44b252e0dc32
linasama6000 <94985065+linasama6000@users.noreply.github.com>
2021-12-06 19:54:48 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,31 @@
 function compute()
 {
+    
     var principal = document.getElementById("principal").value;
+    if(principal<1){
+        alert("Enter a positive number");
+        return false;
+        console.log(principal);
+        
+    }
     var rate = document.getElementById("rate").value;
+    console.log(rate);
     var years = document.getElementById("years").value;
-    var interest = principal * years * rate /100;
-    var year = new Date().getFullYear()=+parseInt(years);    
+    console.log(years);
+    var  interest = principal*years*rate/100;
+    console.log(interest);
+    var year = new Date().getFullYear()+parseInt(years);
+    console.log(year);
+    var res = "If you deposit, <span class = 'highlight'>" + principal+
+            "</span> ,<br> at an interest rate of <span class = 'highlight'>" +rate+
+            "</span>. <br> You will receive an amount of <span class = 'highlight'>" +interest+
+            "</span>, <br> in the year <span class = 'highlight'>"+year;
+  document.getElementById("result").innerHTML = res;
+    console.log("inner html executed");
 }
+
 function updateRate()
-{ var rateval = document.getElementById("rate").value;
-document.getElementById("rate_val").innerText=rateval
-document.getElementById("result").innerHTML="If you deposit "1000000",\<br\>at an interest rate of "3.5"%\<br\>You will receive an amount of "175000",\<br\>in the year "2025"\<br\>"
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
 }
